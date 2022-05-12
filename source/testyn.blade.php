c58fd91e4ac98a585c45f3f08b0c0e73aa567b8d
Rafael Hernandez <rafael@koalaworkshop.com>
2022-05-12 14:38:18 -0500

    Test YN

---
+++ source/testyn.blade.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+<script type="text/javascript" src="https://static.younoodle.com/static/shared/scripts/embed.min.js"></script>
+<script type="text/javascript">
+    YouNoodleEmbed.init({
+        iframe: 'https://platform.younoodle.com/dashboard/competitions/younoodle_demo_2018',
+        width: 1200,
+        height: 720
+    });
+</script>
+</body>
+</html>
