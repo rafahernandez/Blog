f1f00f59e5eaa0feaff7a6d68985dd4562d38c07
Jose Rafael Hernandez <joraheta@gmail.com>
2022-11-15 13:33:19 -0600

    Update testyn.blade.php

--- source/testyn.blade.php
+++ source/testyn.blade.php
@@ -11,7 +11,7 @@
 <script type="text/javascript" src="https://static.younoodle.com/static/shared/scripts/embed_v5.js"></script>
 <script type="text/javascript">
     YouNoodleEmbed.init({
-        iframe: 'https://platform.younoodle.com/competition/the_black_ambition_prize_2022/public-vote/ba_pv_competition',
+        iframe: 'https://yn-accounts-staging.firebaseapp.com/login?redirect-url=https://staging-platform.younoodle.com/dashboard',
         width: 1200,
         height: 720,
         scrolling: 'yes'
